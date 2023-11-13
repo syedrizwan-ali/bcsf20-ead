322039c5279590a9c8ce16cd42176ac9147965e0
Syed Rizwan Ali <s.rizwan@ssidecisions.com>
2023-11-13 17:10:41 +0500

    13 November 23 - Lecture Repository Pattern

---
+++ PostMid/RepoPatternsADO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoPatternsADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
